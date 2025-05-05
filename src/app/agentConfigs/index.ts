b664aaf3836422e2a67dbe3bbdf4680a2852613e
NischalRam <nischalvooda@gmail.com>
2025-05-05 15:42:10 -0500

    sync

--- src/app/agentConfigs/index.ts
+++ src/app/agentConfigs/index.ts
@@ -3,8 +3,9 @@ import frontDeskAuthentication from "./frontDeskAuthentication";
 import customerServiceRetail from "./customerServiceRetail";
 import simpleExample from "./simpleExample";
 import jarvis from "./jarvis";
-import friday from "./TravelAgent"
-import harmony from "./HRPike"
+import friday from "./TravelAgent";
+import harmony from "./HRPike";
+
 
 export const allAgentSets: AllAgentConfigsType = {
   frontDeskAuthentication,
